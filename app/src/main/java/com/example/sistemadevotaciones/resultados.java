a44cb83057bb9a2741ca456634448240b6aee78e
Alej4ndra <alejandra.caballero@utp.ac.pa>
2022-09-22 01:38:13 -0500

    desarrollo de pantalla resultado, error contador resultado

--- app/src/main/java/com/example/sistemadevotaciones/resultados.java
+++ app/src/main/java/com/example/sistemadevotaciones/resultados.java
@@ -2,16 +2,39 @@ package com.example.sistemadevotaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class resultados extends AppCompatActivity {
+
+    TextView candidato1, candidato2, candidato3, votonulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultados);
 
+        candidato1 = (TextView) findViewById(R.id.candidato1);
+        candidato2 = (TextView) findViewById(R.id.candidato2);
+        candidato3 = (TextView) findViewById(R.id.candidato3);
+        votonulo = (TextView) findViewById(R.id.votonulo);
 
-        //graficos
+        Intent intent = getIntent();
+
+        Integer cont_1 = getIntent().getIntExtra("cont_candidato1", 0);
+        Integer cont_2 = getIntent().getIntExtra("cont_candidato2", 0);
+        Integer cont_3 = getIntent().getIntExtra("cont_candidato3", 0);
+        Integer cont_nulo = getIntent().getIntExtra("cont_nulo", 0);
+
+        String cand_1 = String.valueOf(cont_1);
+        String cand_2 = String.valueOf(cont_2);
+        String cand_3 = String.valueOf(cont_3);
+        String nulo = String.valueOf(cont_nulo);
+
+        candidato1.setText(cand_1);
+        candidato2.setText(cand_2);
+        candidato3.setText(cand_3);
+        votonulo.setText(nulo);
     }
 }
--- app/src/main/java/com/example/sistemadevotaciones/votacion.java
+++ app/src/main/java/com/example/sistemadevotaciones/votacion.java
@@ -20,7 +20,7 @@ public class votacion extends AppCompatActivity {
 
 
     //contadores de votacion
-    int candidato1_VivianValencuela = 0, candidato2_OmarAizpurua = 0, candidato3_MartinCandenado = 0, voto_Nulo =0;
+    int candidato1_VivianValencuela, candidato2_OmarAizpurua, candidato3_MartinCandenado, voto_Nulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
